4747c905cb71a44bb97235c65c100524574e09d1
Hakim <hakim@Hakims-MacBook-Pro.local>
2018-07-29 19:58:39 -0400

    First commit

---
+++ addonrmsapp/src/main/java/com/addonusa/rmsapp/AddonrmsappApplication.java
@@ -0,0 +1,12 @@
+package com.addonusa.rmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddonrmsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddonrmsappApplication.class, args);
+	}
+}
---
+++ addonrmsapp/src/main/java/com/addonusa/rmsapp/HomeController.java
@@ -0,0 +1,15 @@
+package com.addonusa.rmsapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sun.javafx.collections.MappingChange.Map;
+
+@Controller
+public class HomeController {
+	
+	  @RequestMapping("/")
+	    public String homett(Map<String, Object> model) {
+	        return "hi";
+	    }
+	}
